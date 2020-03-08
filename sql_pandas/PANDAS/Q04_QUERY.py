7690cbc3030a404162befa63848dfa41e7880e09
MouraYuri <mourayuri339@gmail.com>
2020-03-07 23:14:59 -0300

    adsa

--- sql_pandas/PANDAS/Q04_QUERY.py
+++ sql_pandas/PANDAS/Q04_QUERY.py
@@ -1,21 +1,20 @@
 import pandas as pd 
 import numpy as np 
 
-
-
 df = pd.read_csv('./sql_pandas/PANDAS/dataset/titanic.csv')
 
-indexes1 = np.where(df.loc[:,'Age'] <= 18)
+criançasIdx = np.where(df.loc[:,'Age'] <= 18)
 
-indexes2 = np.where(df.loc[:,'Age'] > 18)
-indexes3 = np.where(df.loc[:,'Age'] > 65)
+idosoIdx = np.where(df.loc[:,'Age'] > 65)
+
+for x in criançasIdx: df.loc[x, 'Age'] = "Criança"
+
+for x in range(len(df)):
+    if x not in criançasIdx[0] and x not in idosoIdx[0]:
+        df.loc[x, 'Age'] = "Adulto"
+
+for x in idosoIdx: df.loc[x, 'Age'] = "Idoso"
 
 
-idx = indexes1 + tuple([[4 for x in range(len(indexes1[0]))]])
-print(df.iloc[idx])
-
-#df.iloc[np.where(df.loc[:,'Age']<=18)] = "Criança"
-#df.iloc[indexes] = "Criança"
-
-
-#print('result',result)
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+    print(df.loc[:]['Age'])
