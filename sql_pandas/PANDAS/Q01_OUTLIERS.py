2f4e67c209507325558ad0eb3c8a72fcde2d00ef
MouraYuri <mourayuri339@gmail.com>
2020-03-08 14:34:59 -0300

    assdas

--- sql_pandas/PANDAS/Q01_OUTLIERS.py
+++ sql_pandas/PANDAS/Q01_OUTLIERS.py
@@ -1,2 +1,15 @@
 import pandas as pd 
+from matplotlib import pyplot as plt 
 
+
+df = pd.read_csv('./sql_pandas/PANDAS/dataset/iris.data')
+
+'''
+We can verify if the dataset has outliers with boxplots
+'''
+for x in range(len(df.columns)-1):
+    plt.figure()
+    plt.boxplot(df.iloc[:,x])
+
+
+plt.show()
