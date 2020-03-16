f73364907ad34cb182b0c5714c88fdbed2d44574
MouraYuri <mourayuri339@gmail.com>
2020-03-16 20:14:46 -0300

    =]

--- sql_pandas/PANDAS/Q01_OUTLIERS.py
+++ sql_pandas/PANDAS/Q01_OUTLIERS.py
@@ -6,6 +6,12 @@ df = pd.read_csv('./sql_pandas/PANDAS/dataset/iris.data')
 
 '''
 We can verify if the dataset has outliers with boxplots
+
+This technique uses interquartile range rule:
+    Points above Q3 + 1.5×IQR 
+    AND
+    Points below Q1 – 1.5×IQR
+    Are considered outliers
 '''
 for x in range(len(df.columns)-1):
     plt.figure()
